fa590de0fa92c03140d940ac01b5c794339b0411
carloshenriquehannas <carloshenriquehannas@gmail.com>
2023-09-04 12:06:31 -0300

    Upload dos arquivos da atividade 4

---
+++ sel0620_lab4.m
@@ -0,0 +1,71 @@
+close all;
+%clear all;
+
+%Constantes dadas
+zeta = 1.011;
+wn = 1.093;
+R = 1.15;
+Kp = 1;
+G = tf(wn^2, [1 2*zeta*wn wn^2]); %Funcao de Transferencia em malha aberta
+
+wb = bandwidth(G); %Largura de banda em rad/s
+f = wb/(2*pi); %Largura de banda em Hz
+
+ws = 35*wb; 
+T0 = (2*pi)/ws; %Periodo de amostragem
+
+Gz = c2d(G, T0, 'zoh'); %Funcao de transferencia no dominio Z
+
+%Recupera G para Simulink
+[num, den] = tfdata(G, 'v');
+
+%Plot do resultado do Simulink da saida y
+figure
+plot(out.y_c.Time, out.y_c.Data, 'b')
+hold on
+stairs(out.y_d.Time, out.y_d.Data, 'r')
+legend(['continuo';'discreto']);
+title('Sinal de saída y do sistema contínuo e discreto, com Simulink.')
+xlabel('Time (seconds)')
+ylabel('Amplitude da saída y(t)')
+
+%Plot do resultado do Simulink da entrada da planta
+figure
+plot(out.u_c.Time, out.u_c.Data, 'b')
+hold on
+stairs(out.u_d.Time, out.u_d.Data, 'r')
+legend(['continuo';'discreto']);
+title('Sinal de entrada da planta do sistema contínuo e discreto, com Simulink.')
+xlabel('Time (seconds)')
+ylabel('Amplitude da entrada u(t)')
+
+%Plot do resultado do Simulink do erro
+figure
+plot(out.e_c.Time, out.e_c.Data, 'b')
+hold on
+stairs(out.e_d.Time, out.e_d.Data, 'r')
+legend(['continuo';'discreto']);
+title('Sinal do erro e do sistema contínuo e discreto, com Simulink.')
+xlabel('Time (seconds)')
+ylabel('Amplitude do erro e(t)')
+
+%Root Locus
+figure
+rlocus(G)
+
+%Diagrama de Bode e margens de ganho/fase
+figure
+bode(G)
+margin(G)
+
+%Funcao de transferencia de malha fechada do sistema continuo e discreto
+Gmf = feedback (Kp*G, 1);
+Gmfz = feedback (Kp*Gz , 1);
+
+%Polos e zeros das funcoes de transferencia em malha fechada
+p = pole(Gmf);
+z = zero(Gmf);
+
+dp = pole(Gmfz);
+dz = zero(Gmfz);
+
