56283373cb104a7f1278aa3322fc30ca54eb250d
carloshenriquehannas <carloshenriquehannas@gmail.com>
2023-11-08 11:41:04 -0300

    Finalizacao pratica 11

--- Pratica11/pratica11.m
+++ Pratica11/pratica11.m
@@ -16,3 +16,28 @@ T0 = (2*pi)/ws; %Periodo de amostragem
 
 Gmf = feedback(Kp*G, 1); %Funcao de transferencia em malha fechada
 
+k1 = place(F, H, [0.7703+0.1452i, 0.7703-0.1452i]);
+
+%Plot do resultado do Simulink do espaco de estado
+figure
+stairs(out.x_d.Time, out.x_d.Data, 'r')
+legend(['discreto x1'; 'discreto x2']);
+title('Sinal de estados do sistema contínuo e discreto, com Simulink.')
+xlabel('Time (seconds)')
+ylabel('Estados do Sistema')
+
+%Plot do resultado do Simulink da saida y
+figure
+stairs(out.y_d.Time, out.y_d.Data, 'r')
+legend(['discreto']);
+title('Sinal da saída y do sistema , com Simulink.')
+xlabel('Time (seconds)')
+ylabel('Amplitude da saída')
+
+%Plot do resultado do Simulink da entrada u
+figure
+stairs(out.u_d.Time, out.u_d.Data, 'r')
+legend(['discreto']);
+title('Sinal da entrada u, com Simulink.')
+xlabel('Time (seconds)')
+ylabel('Amplitude da entrada')
