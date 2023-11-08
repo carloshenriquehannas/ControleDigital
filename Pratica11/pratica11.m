15141383276a1d6c7e38a3e0950ca669f3e98ea5
carloshenriquehannas <carloshenriquehannas@gmail.com>
2023-11-08 09:44:28 -0300

    Insercao em malha fechada

--- Pratica11/pratica11.m
+++ Pratica11/pratica11.m
@@ -8,3 +8,11 @@ R = 1.15;
 Kp = 8;
 
 G = tf(wn^2, [1 2*zeta*wn wn^2]); %Funcao de transferencia da planta
+
+wb = bandwidth(G); %Largura de banda em rad/s
+f = wb/(2*pi); %Largura de banda em Hz
+ws = 35*wb; 
+T0 = (2*pi)/ws; %Periodo de amostragem 
+
+Gmf = feedback(Kp*G, 1); %Funcao de transferencia em malha fechada
+
